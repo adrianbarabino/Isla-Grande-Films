2cd76063b899a14d6385a7f465afd7dcae7efed5
Adrian Barabino <adrian.barabino@gmail.com>
2014-02-19 20:51:25 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html> <!-- Define el tipo de doc -->
+<html lang="en"> <!-- Inicia documento HTML con idioma EN -->
+<head> <!-- Cabecera del documento -->
+	<meta charset="UTF-8"> <!-- Metadatos: Define que el charset es UTF-8 -->
+	<title>Document</title> <!-- Titulo -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800|Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body> <!-- Inicia el cuerpo de la página -->
+
+	<section id="contenedor">
+		
+		<nav>
+			<ul id="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Quienes Somos</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+
+		<header>
+			<h1>Isla Grande Films</h1>
+			<p>Isla Grande Films es un proyecto de Productora Audiovisual Independiente, creada en la Patagonia Argentina.</p>
+		</header>
+
+		<section id="proyectos">
+			<article id="earthship">
+				<h2>Earthship Ushuaia Entrevista Michael Reynolds</h2>
+				<p>Ut nisl augue, commodo vel facilisis id, blandit nec tortor. Morbi eget dolor blandit, dictum ipsum in, bibendum risus. Sed tempor sagittis tortor at placerat. Sed porttitor tortor nec condimentum consequat. Curabitur hendrerit tortor sed neque hendrerit, nec facilisis sem congue. Integer tempus dictum orci. Sed et justo ultrices, varius odio a, porta magna.</p>
+			</article>
+			<article id="historias">
+				<h2>Historias del Alto Vuelo – Aeroclub Río Gallegos</h2>
+				<p>Morbi pulvinar, mi a mollis mattis, purus leo scelerisque enim, nec blandit turpis arcu in risus. Morbi semper ut tortor in sodales. Mauris lobortis tellus in vehicula volutpat. Nam nec est in nibh posuere facilisis vitae suscipit justo. Suspendisse eget odio a nibh tristique tristique. Curabitur fringilla gravida erat. Maecenas fermentum, est quis placerat aliquet, libero justo posuere metus, in consectetur nulla lorem sit amet tortor. Nunc vitae nulla iaculis.</p>
+			</article>
+		</section>
+
+		<footer>
+			<span><a href="#">Sitio realizado por Adrian Barabino</a></span>
+		</footer>
+	</section>
+	
+
+</body>
+</html>
