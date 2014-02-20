df4ee87c569d6268cf3251a5e9cb4e518cbeaa0a
Adrian Barabino <adrian.barabino@gmail.com>
2014-02-20 20:04:31 -0300

    Pruebas con JS

--- index.php
+++ index.php
@@ -3,24 +3,24 @@
 <head> <!-- Cabecera del documento -->
 	<meta charset="UTF-8"> <!-- Metadatos: Define que el charset es UTF-8 -->
 	<title>Document</title> <!-- Titulo -->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800|Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="estilo.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800|Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'> <!-- Google Web Fonts -->
+	<link rel="stylesheet" href="estilo.css"> <!-- Insertamos el estilo CSS -->
 </head>
-<body> <!-- Inicia el cuerpo de la página -->
+<body onload="iniciar();"> <!-- Inicia el cuerpo de la página -->
 
 	<section id="contenedor">
 		
 		<nav>
 			<ul id="menu">
-				<li><a href="#">Inicio</a></li>
-				<li><a href="#">Quienes Somos</a></li>
-				<li><a href="#">Blog</a></li>
-				<li><a href="#">Contacto</a></li>
+				<li><a href="javascript:cargarPagina('inicio');">Inicio</a></li>
+				<li><a href="javascript:cargarPagina('quienes-somos');">Quienes Somos</a></li>
+				<li><a href="javascript:cargarPagina('blog');">Blog</a></li>
+				<li><a href="javascript:cargarPagina('contacto');">Contacto</a></li>
 			</ul>
 		</nav>
 
 		<header>
-			<h1>Isla Grande Films</h1>
+			<h1 id="titulo">Isla Grande Films</h1>
 			<p>Isla Grande Films es un proyecto de Productora Audiovisual Independiente, creada en la Patagonia Argentina.</p>
 		</header>
 
@@ -40,6 +40,6 @@
 		</footer>
 	</section>
 	
-	<script src="principal.js"></script>
+	<script src="principal.js"></script> <!-- Invocamos nuestro script JS -->
 </body>
 </html>
--- principal.js
+++ principal.js
@@ -1,1 +1,25 @@
-console.log("Script!");
+function iniciar () {
+	console.log("Iniciamos la aplicacion");
+}
+
+function cargarPagina (pagina) {
+	switch(pagina)
+	{
+		case 'inicio': 
+			color = "#67b7e1";
+			break;
+		case 'quienes-somos':
+			color = "#b767e1";
+			break;
+		case 'blog':
+			color = "#e167b7";
+			break;
+		case 'contacto':
+			color = "#000";
+			break;
+		default: 
+			color = "#67b7e1";
+	}
+	document.getElementById("titulo").innerHTML = pagina;
+	document.getElementsByTagName('nav')[0].style.background = color;
+}
